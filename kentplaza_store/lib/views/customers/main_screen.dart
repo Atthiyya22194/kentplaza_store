b927b8478f109718d04801ac362c1bd4c15a3f5c
Atthiyyath <56459274+Atthiyya22194@users.noreply.github.com>
2022-11-28 16:51:35 +0300

    BottomNavigation
    but still error

--- kentplaza_store/lib/views/customers/main_screen.dart
+++ kentplaza_store/lib/views/customers/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -10,6 +11,52 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.black,
+        selectedItemColor: Colors.yellow.shade900,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: 'HOME',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/explore.svg',
+              width: 20,
+            ),
+            label: 'CATEGORIES',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/shop.svg',
+              width: 20,
+            ),
+            label: 'STORE',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/cart.svg',
+              width: 20,
+            ),
+            label: 'CART',
+          ),
+          BottomNavigationBarItem(
+            icon: new SvgPicture.asset(
+              'assets/icons/search.svg',
+              width: 20,
+            ),
+            label: 'SEARCH',
+          ),
+          BottomNavigationBarItem(
+            icon: SvgPicture.asset(
+              'assets/icons/account.svg',
+              width: 20,
+            ),
+            label: 'ACCOUNT',
+          ),
+        ],
+      ),
+    );
   }
 }
